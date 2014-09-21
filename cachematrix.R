64bdc9a0d6c36f21de019d3ffbfa57d5700dfc73
pprold <your_email@example.com>
2014-09-21 14:30:33 -0400

    implement matrix inverse with caching - ProgAssignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Compute the inverse of a matirx caching the value of calculations
+## Example of using scope assignment operator "<<-"
 
-## Write a short comment describing this function
-
+## Create a special object to store the matrix and the cached inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## If the value had been computed before, the cached inverse is returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    ## check if the inverse was cached
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    ## do the calculation
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
